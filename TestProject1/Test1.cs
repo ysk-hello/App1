c89f28ad02d6cbe76679878b5b5edff53945f27e
ysk-hello <hello.ysk1905@gmail.com>
2025-06-18 08:58:04 +0900

    新規登録

---
+++ TestProject1/Test1.cs
@@ -0,0 +1,17 @@
+﻿using App1.Services;
+
+namespace TestProject1
+{
+    //https://learn.microsoft.com/ja-jp/windows/apps/winui/winui3/testing/
+
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var service = new TestService();
+            Assert.AreEqual(-1, service.GetCount());
+        }
+    }
+}
---
+++ TestProject2/Test1.cs
@@ -0,0 +1,17 @@
+﻿using App1.Services;
+
+namespace TestProject2
+{
+    //dotnet tool install -g dotnet-reportgenerator-globaltool
+
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var service = new TestService();
+            Assert.AreEqual(-1, service.GetCount());
+        }
+    }
+}
